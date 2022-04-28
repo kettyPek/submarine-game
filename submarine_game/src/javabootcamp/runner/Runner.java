2cfe022c6146b46625867eb0bfe426dcfed141db
kettyPek <ketty.pekarsky@gmail.com>
2022-04-28 16:09:46 +0300

    runner updated

--- submarine_game/src/javabootcamp/runner/Runner.java
+++ submarine_game/src/javabootcamp/runner/Runner.java
@@ -1,5 +1,6 @@
 package javabootcamp.runner;
 
+
 import javabootcamp.game.Game;
 
 public class Runner {
@@ -7,6 +8,6 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		Game game = new Game();
+		game.startdGame();
 	}
-
 }
