88ae3f380c18732bddcc2e2d259db78fbbe816d1
kettyPek <ketty.pekarsky@gmail.com>
2022-05-09 09:54:04 +0300

    displayMainMenu method added

--- submarine_game/src/javabootcamp/menu/Menu.java
+++ submarine_game/src/javabootcamp/menu/Menu.java
@@ -1,11 +1,52 @@
 package javabootcamp.menu;
 
-public abstract class Menu {
+import javabootcamp.game.Game;
+import javabootcamp.player.Player;
+import javabootcamp.utils.GameScanner;
+
+public class Menu {
 	
 	public static boolean gameOn ;
+	public static boolean replayMode ;
+	public static int gameCounter;
 	
 	static {
-		gameOn = true;
+		gameOn = false;
+		replayMode = false;
+		gameCounter = 0;
+	}
+	
+	public void displayMainMenu() {
+		System.out.println("Please select:");
+		System.out.println("1 - play new game");
+		System.out.println("2 - replay a game");
+		enterModeByUserSeLection();
+	}
+	
+	private void enterModeByUserSeLection() {
+		int selection = GameScanner.scanner.nextInt();
+		switch(selection){
+		case 1:
+			gameOn = true;
+			replayMode = false;
+			gameCounter++;
+			Player player1 = new Player("ketty","email@mailcom",0546050505);
+			Game game = new Game(player1);
+			game.startdGame();
+			break;
+		case 2:
+			gameOn = false;
+			replayMode = true;
+			displayReplayMenu();
+			break;
+		}	
+	}
+	
+	private void displayReplayMenu() {
+		System.out.println("Please select game to replay:");
+		int selection = GameScanner.scanner.nextInt();
+		String fileToReplay = "GameFiles/gameNumber" + selection + ".dat";
+		//replay game function
 	}
 	
 	
